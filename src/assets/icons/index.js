74e5fd9774919b366dc0bfc036d983401c8dc16c
Almog Shoob <almogshoob1@gmail.com>
2025-09-14 22:53:50 +0300

    init

--- src/assets/icons/index.js
+++
@@ -1,6 +0,0 @@
-import KeyboardIcon from "../../assets/icons/keyboard.svg?react";
-import DarkModeIcon from "../../assets/icons/moon-outlined.svg?react";
-import LightModeIcon from "../../assets/icons/sun-outlined.svg?react";
-
-export { DarkModeIcon, KeyboardIcon, LightModeIcon };
-
