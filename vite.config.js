09f1b06d5190085cb17933de5486ca74c5782588
Almog Shoob <almogshoob1@gmail.com>
2025-09-14 23:04:56 +0300

    fix vite config base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/color-guesser/",
+  base: "/hsl-vs-rgb/",
   plugins: [react(), svgr()],
 })
